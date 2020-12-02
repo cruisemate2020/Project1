2571f2e17b5b83e5c820f9e35f807eb3736adb1c
Valerie Cevallos <valerie.cevallos@gmail.com>
2020-12-02 16:18:51 -0500

    more updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
+//global variables
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 const keys = document.querySelectorAll('.key');
 const BPM = 130;
-const gameLevel = .5;
+const gameLevel = 2;
 const BPMTime = Math.floor (gameLevel * 1000 * 60 * (1 / BPM));
 const backgroundImg = new Image();
-backgroundImg.src = 'images/colorStaff.jpg'
+backgroundImg.src = '/images/colorStaff.png';
 
 let interval;
 let showedNote = 0;
@@ -15,16 +16,15 @@ let score = 0;
 let gameBtn = document.getElementById('playGame');
 let freePlayBtn = document.getElementById('freePlay');
 
-
+//page load
 window.onload = () => {
-    // console.log('playgame');
     document.getElementById('playGame').onclick = () => {
-    // console.log('onclick');
     interval = window.setInterval(incSongNote, BPMTime);
     renderCanvas();
     };
 }
 
+//piano key display functionality and visuals
 function playNote(note) {
     let keySound;
     keySound = document.getElementById(note);
@@ -42,88 +42,82 @@ function keyReturn(note) {
     endKey.classList.remove('playing');
 }
 
-//keyboard manipulation
+//piano key / keyboard associations
 const timer = 500;
 
 document.addEventListener('keydown', event => {
 switch(event.code) {
-    case 'KeyZ':
+    case 'KeyA':
         playKeyboard('C2');
         break;
-    case 'KeyS':
+    case 'KeyW':
         playKeyboard('Db2');
         break;
-    case 'KeyX':
+    case 'KeyS':
         playKeyboard('D2');
         break;
-    case 'KeyD':
+    case 'KeyE':
         playKeyboard('Eb2');
         break;
-    case 'KeyC':
+    case 'KeyD':
         playKeyboard('E2');
         break;    
-    case 'KeyV':
+    case 'KeyF':
         playKeyboard('F2');
         break;
-    case 'KeyG':
+    case 'KeyT':
         playKeyboard('Gb2');
         break;
-    case 'KeyB':
+    case 'KeyG':
         playKeyboard('G2');
         break;
-    case 'KeyH':
+    case 'KeyY':
         playKeyboard('Ab2');
         break;
-    case 'KeyN':
+    case 'KeyH':
         playKeyboard('A2');
         break;
-    case 'KeyJ':
+    case 'KeyU':
         playKeyboard('Bb2');
         break;
-    case 'KeyM':
+    case 'KeyJ':
         playKeyboard('B2');
         break;
-    case 'Comma':
+    case 'KeyK':
         playKeyboard('C3');
         break;
-    case 'KeyL':
+    case 'KeyO':
         playKeyboard('Db3');
         break;
-    case 'Period':
+    case 'KeyL':
         playKeyboard('D3');
         break;
-    case 'Semicolon':
+    case 'KeyP':
         playKeyboard('Eb3');
         break;
-    case 'Slash':
+    case 'Semicolon':
         playKeyboard('E3');
         break;
 }
 })
 
 function playKeyboard (keyNote) {
-    // console.log('playkeyboard');
     playNote(keyNote);
     keyChange(keyNote);
     setTimeout(() => { keyReturn(keyNote); }, timer);
     if (checkedNote === keyNote) {
         score++;
-        // console.log(`---> score ${score}`);
     }
 }
 
-// //draw notes
-// const noteImgs = ['C2', 'Db2', 'D2', 'Eb2', 'E2', 'F2', 'Gb2', 'G2', 'Ab2', 'A2', 'Bb2', 'B2', 'C3', 'Db3', 'D3b', 'Eb3', 'E3'];
+//Here Comes The Sun 
+//const songNotes = ['C2', 'F2', 'E2', 'F2', 'A2', 'G2', 'C3', 'A2', 'C3', 'D3', 'C3', 'A2', 'G2', 'E3', 'C3', 'D3', 'E3', 'E3', 'D3', 'C3', 'A2', 'E3', 'D3', 'C3', 'B2', 'E3', 'D3', 'E3', 'C3', 'E3', 'C3', 'D3', 'E3', 'E3', 'D3', 'E3', 'C3', 'C3', 'D3', 'C3', 'E3', 'D3', 'C3'];
 
-const songNotes = ['E3', 'C3', 'D3', 'E3', 'E3', 'D3', 'C3', 'A2', 'C3', 'D3', 'C3', 'A2', 'G2', 'E3', 'C3', 'D3', 'E3', 'E3', 'D3', 'C3', 'A2', 'E3', 'D3', 'C3', 'B2', 'E3', 'D3', 'E3', 'C3', 'E3', 'C3', 'D3', 'E3', 'E3', 'D3', 'E3', 'C3', 'C3', 'D3', 'C3', 'E3', 'D3', 'C3'];
+//Auld Lang Syne
+const songNotes = ['C2', 'F2', 'E2', 'F2', 'A2', 'G2', 'F2', 'G2', 'A2', 'F2', 'F2', 'A2', 'C3', 'D3', 'D3', 'C3', 'A2', 'A2', 'F2', 'G2', 'F2', 'G2', 'A2', 'G2', 'F2', 'D2', 'D2', 'C2', 'F2', 'D3', 'C3', 'A2', 'A2', 'F2', 'G2', 'F2', 'G2', 'D3', 'C3', 'A2', 'A2', 'C3', 'D3', 'D3', 'C3', 'A2', 'A2', 'F2', 'G2', 'F2', 'G2', 'A2', 'G2', 'F2', 'D2', 'D2', 'C2', 'F2']
 
-
-//need to create method to create start and freeplay... start button to call render Canvas, free play to call method to play free notes
-
-// renderCanvas();
-
-
-
+//game functionality (notes displaying on the staff at given interval)
+//(option to have user change speed with selector in next version))
 function incSongNote() {
     console.log('incSongNote')
     notePosition -= 100;
@@ -153,7 +147,6 @@ function renderCanvas()
 
         if (startingNotePosition > 375 && startingNotePosition < 500) {
         checkedNote = n.src.substr(n.src.length-6, 2);
-        // console.log(`--->${checkedNote}`);
         }
         startingNotePosition += 100;
     });
@@ -166,25 +159,28 @@ function renderCanvas()
 }
 
 function showScore() {
-    // console.log('showScore');
     context.fillStyle = 'black';
     context.font = '40px "Luckiest Guy"';
     context.fillText(`Score: ${score}`, 800, 50);
 }
 
 function gameOver() {
-    // console.log('gameOver');
     context.clearRect(0, 0, 1000, 350);
     context.shadowBlur = 10;
     context.shadowOffsetY = 10;
-    context.shadowColor = "lightgreen";
+    context.shadowColor = 'lightgreen';
     context.fillStyle = 'blue';
     context.font = '90px "Luckiest Guy"';
-    context.fillText(`GAME OVER!`, 300, 150);
+    context.fillText(`GAME OVER!`, 260, 150);
+    context.lineWidth = 1.5;
+    context.strokeStyle = "lightgreen"; 
+    context.strokeText("GAME OVER!", 260, 150); 
     context.fillStyle = 'blue';
     context.font = '70px "Luckiest Guy"';
-    context.fillText(`You scored ${score} points!`, 175, 250);
-    gameBtn.innerHTML = 'Play Again';
+    context.fillText(`You scored ${score} points!`, 160, 250);
+    context.strokeText(`You scored ${score} points!`, 160, 250); 
+    gameBtn.innerHTML = 'Reset Game';
     gameBtn.addEventListener('click', () => {location.reload();
+    window.cancelAnimationFrame(renderCanvas);
     })
 }
